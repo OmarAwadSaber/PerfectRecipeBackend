7bb2ab15fe7a3ca3af0179d48370a1e32f4fe758
Zyad <ziadashraf950@gmail.com>
2025-05-15 23:53:36 +0300

    Add Recipe

--- perfectRecipe/Recipes/models.py
+++ perfectRecipe/Recipes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    id = models.IntegerField(primary_key=True)
+    image = models.ImageField(upload_to='recipes/',null=True,blank=True)
+    description = models.TextField()
+    course_name = models.CharField(max_length=200)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+
+
+    def __str__(self):
+        return self.name
+    
+
--- perfectRecipe/Recipes/urls.py
+++ perfectRecipe/Recipes/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from .views import RecipeListAPI, RecipeDetailAPI
 
 urlpatterns = [
-
+    path('', RecipeListAPI.as_view(), name='recipe-list'),
+    path('<int:pk>/', RecipeDetailAPI.as_view(), name='recipe-detail'),
 ]
