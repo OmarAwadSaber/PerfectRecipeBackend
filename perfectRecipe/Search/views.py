b15dc71ad75442477ffede003ddaa83c0e3ef799
AmrKhaled35 <amrk6965@gmail.com>
2025-05-15 19:36:07 +0300

    Add : Search View and Url

--- perfectRecipe/Search/views.py
+++ perfectRecipe/Search/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+from django.db.models import Q
+from recipes.models import Recipe
+from recipes.serializers import RecipeSerializer
+class SearchView(generics.ListAPIView):
+    serializer_class = RecipeSerializer
+    def get_queryset(self):
+        queryset = Recipe.objects.all()
+        search_query = self.request.query_params.get('search')
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(ingredients__icontains=search_query)
+            )
+        return queryset
 
-# Create your views here.
